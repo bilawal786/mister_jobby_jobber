a8b0c0294de431dee199fc577149ed2c00de3d78
Hassan <hassan@codingcrust.com>
2022-10-10 17:36:50 +0500

    const provider add, time availability screen, group radio tile widget, etc added, some little changes

--- lib/screens/auth_screens/mandatory_steps/reliability_score_step/reliability_score_step_screen.dart
+++ lib/screens/auth_screens/mandatory_steps/reliability_score_step/reliability_score_step_screen.dart
@@ -1,7 +1,8 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:mister_jobby_jobber/widgets/const_widgets/custom_button.dart';
-import 'package:mister_jobby_jobber/widgets/const_widgets/custom_list_tile.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import '../../../../widgets/const_widgets/custom_button.dart';
+import '../../../../widgets/const_widgets/custom_list_tile.dart';
 
 class ReliabilityScoreScreen extends StatelessWidget {
   const ReliabilityScoreScreen({Key? key}) : super(key: key);
