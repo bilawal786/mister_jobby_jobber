fa06bc62c623d158d5c93df4fceec361cc76242f
mujeebcc <mujeeb@codingcrust.com>
2022-10-31 13:55:19 +0500

    comments

--- lib/providers/jobs_providers/single_job_comments_provider.dart
+++ lib/providers/jobs_providers/single_job_comments_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:mister_jobby_jobber/providers/mandatory_steps_provider/personal_information_provider/personal_information_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -17,7 +18,7 @@ class SingleJobCommentsProvider with ChangeNotifier {
     final SharedPreferences sharePref = await SharedPreferences.getInstance();
     String? userToken = sharePref.getString('token');
     var response = await http.get(
-      Uri.parse('${MyRoutes.BASEURL}/getComments/$jobId'),
+      Uri.parse('${MyRoutes.BASEURL}/demandeur/getComments/$jobId'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Accept': 'application/json',
@@ -34,36 +35,37 @@ class SingleJobCommentsProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  // void postSingleComment(BuildContext context, SingleJobCommentsModel sComments, int jobId) async {
-  //   SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
-  //   String? userToken = sharedPrefs.getString("token");
-  //   var response = await http.post(
-  //     Uri.parse('${MyRoutes.BASEURL}/comments'),
-  //     headers: <String, String>{
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //       'Accept': 'application/json',
-  //       'Authorization': 'Bearer ${userToken!}',
-  //     },
-  //     body: jsonEncode(<String, String>{
-  //       'job_id': jobId.toString(),
-  //       'comment': sComments.message
-  //     }),
-  //   );
-  //   if (response.statusCode == 200) {
-  //     final newComment = SingleJobCommentsModel(
-  //         id: 0,
-  //         userId: profileData!.userId,
-  //         name: "${profileData.firstName} ${profileData.lastName}",
-  //         image: profileData.image,
-  //         message: sComments.message,
-  //         date: sComments.date,
-  //     );
-  //     singleJobComments!.add(newComment);
-  //     print("Comment Post API WORKS");
-  //     notifyListeners();
-  //   } else {
-  //     print("Comment Post API NOT WORKS");
-  //   }
-  //   notifyListeners();
-  // }
+  void postSingleComment(BuildContext context, SingleJobCommentsModel sComments, int jobId) async {
+    final profileData = Provider.of<PersonalInformationProvider>(context).profile;
+    SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
+    String? userToken = sharedPrefs.getString("token");
+    var response = await http.post(
+      Uri.parse('${MyRoutes.BASEURL}/demandeur/comments'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ${userToken!}',
+      },
+      body: jsonEncode(<String, String>{
+        'job_id': jobId.toString(),
+        'comment': sComments.message
+      }),
+    );
+    if (response.statusCode == 200) {
+      final newComment = SingleJobCommentsModel(
+          id: 0,
+          userId: profileData!.jobberId,
+          name: "${profileData.firstName} ${profileData.lastName}",
+          image: profileData.image,
+          message: sComments.message,
+          date: sComments.date,
+      );
+      singleJobComments!.add(newComment);
+      print("Comment Post API WORKS");
+      notifyListeners();
+    } else {
+      print("Comment Post API NOT WORKS");
+    }
+    notifyListeners();
+  }
 }
