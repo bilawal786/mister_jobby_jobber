405ed9c5578af5a8bc4edf6227ba145aaca12e8d
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-11-24 09:51:48 +0500

    Firebase Ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,9 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyDSztKwG57QqCxM-FZ7W8wfDQTlLd1Kc2M")
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
